06d9011822de9efd6de1036e7d52729ed2fadf01
Anton <71210712@students.ukdw.ac.id>
2021-11-12 12:20:23 +0700

    Test Case 2

---
+++ 1_B_71210712.py
@@ -0,0 +1,7 @@
+#1_B_71210712
+
+print("Nama : Uzulan Umurusutu Tyuruk")
+print("Tempat tanggal lahir : Bengkulu 30 Februari 1987")
+print ("Haloo! Tyuruk, Uzulun Umurusutu")
+print("Anda lahir di Jepang pada tanggal 31 November 1987")
+       
