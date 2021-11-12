a9e9029263961c8e313085cacc12b771546571da
Anton <71210712@students.ukdw.ac.id>
2021-11-12 12:10:19 +0700

    Test case 1

---
+++ 1_A_71210712.py
@@ -0,0 +1,7 @@
+#1_A_71210712
+
+print("Nama: Inyint Milyzrin")
+print("Tempat tanggal lahir : Bengkulu 30 Februari 2000")
+print("Haloo! Milzyrin, Inyint")
+print("Anda lahir di Bengkulu pada tanggal 30 Februari 2000")
+
